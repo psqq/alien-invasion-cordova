47e52744179345c83bab2786d4926cc5c8323c70
psqq <pestov-sa@ya.ru>
2020-05-08 12:37:32 +0300

    Screen lock works with delay

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,59 @@
 
 const app = {
+    gameStarted: false,
     main() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
     async onDeviceReady() {
         await this.preload();
         await this.init();
-        this.run();
+        await this.run();
     },
     async preload() {
     },
     async init() {
+        window.screen.orientation.lock('portrait');
     },
-    run() {
+    isPortrait() {
+        return screen.orientation.type === 'portrait' || screen.orientation.type === 'portrait-primary' || screen.orientation.type === 'portrait-secondary';
+    },
+    async run() {
+        this.__startGame();
+    },
+    async __startGame() {
+        if (this.gameStarted) {
+            return;
+        }
+        for (; ;) {
+            try {
+                await this.waitPortrait();
+                break;
+            } catch (e) {
+                alert("Please rotate the device and then click OK");
+            }
+        }
         // eslint-disable-next-line no-undef
         _startGame();
+        this.gameStarted = true;
     },
+    waitPortrait() {
+        return new Promise((res, rej) => {
+            let i = 0;
+            const go = () => {
+                i++;
+                if (i > 3) {
+                    return rej();
+                }
+                const w = window.innerWidth, h = window.innerHeight;
+                if (w > h) {
+                    setTimeout(go, 300);
+                } else {
+                    return res();
+                }
+            };
+            setTimeout(go, 300);
+        });
+    }
 };
 
 export default app;
